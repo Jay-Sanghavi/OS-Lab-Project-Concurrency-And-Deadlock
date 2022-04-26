1d5a9a05e1169bf0f445f4c98ba009ead9a22a8d
techjahnavi <83537545+techjahnavi@users.noreply.github.com>
2022-04-26 15:27:31 +0530

    Update peterson_algo_viz.js

--- JS/peterson_algo_viz.js
+++ JS/peterson_algo_viz.js
@@ -1,44 +1,72 @@
-var lock=0;
+var check = 0; // Counting Semaphore
+var mutex = 1; // Binary Semaphore
+var $output = $("#textArea");
 
-var flag=[0,0]; //flag[0] and flag[1] are false initially for P1 and P2 simaltaneously
-//Time complexity for critical state function is O(1)
-function critical_state(id){
-    var x=document.getElementById(id);
-    x.remove();
-    var y=document.getElementById('critical');
-    if(lock==0){
-        y.innerHTML='<button class=" button" id = '+id+' onclick="exit_state(this.id)">P'+id+'</button>';
-        flag[id-1]=1;
-        lock=1;    
-    
-    }
-    else{
-        alert("Deadlock occures");
-        window.location.reload();
-        lock=0;
-    }
-    
-
+// Up Function
+function down(check, consumed) {
+  check -= consumed;
+  return check;
 }
 
-//Time complexity for exit state is O(1)
-function exit_state(id){
-    var x=document.getElementById(id);
-    x.remove();
-    lock=0;
-    flag[id-1]=0;
-    var y=document.getElementById('exit');
-    y.innerHTML+='<button class="button" id = '+id+' onclick="entry_state(this.id)">P'+id+'</button>';
-    
-
+// Down Function
+function up(check) {
+  check++;
+  return check;
 }
 
-//Time complexity for entry state is O(1)
-function entry_state(id){
-    var x=document.getElementById(id);
-    x.remove();
-    var y=document.getElementById('entry');
-    y.innerHTML+='<button class="button" id = '+id+' onclick="critical_state(this.id)">P'+id+'</button>';
+function producer() {
+  var space = document.getElementById("bufferSize").value;
+  var produced_items = document.getElementById("nProducers").value;
+  var items_added = document.getElementById("added");
+  space++;
 
+  if (mutex == 1) {
+    down(mutex, 1);
+    if (parseInt(check) + parseInt(produced_items) >= space) {
+      alert("Buffer Size should be greater than the items to be produced");
+    } else {
+      for (var i = 1; i <= produced_items; i++) {
+        var o =
+          '<button type="button" class="btn ml-3 my-2 third" id = ' +
+          (check + 1) +
+          ' onclick="add_entry(this.id)">' +
+          "Process" +
+          (check + 1) +
+          "</button>";
+        check = up(check);
+        items_added.innerHTML += o;
+
+        $output.append("Items Produced " + i + "\n");
+      }
+    }
+    up(mutex);
+  }
+}
+
+function consumer() {
+  var space = document.getElementById("bufferSize").value;
+  var consumed_items = document.getElementById("nConsumers").value;
+
+  space++;
+
+  if (mutex == 1) {
+    down(mutex);
+    if (check - consumed_items >= 0) {
+      for (var i = check; i > check - consumed_items; i--) {
+        var x = document.getElementById(i);
+        x.remove();
+        $output.append("Items consumed " + i + "\n");
+      }
+      check = down(check, consumed_items);
+    } else {
+      alert("Items consumed should be less than the items produced");
+    }
+    up(mutex);
+  }
+}
+
+function producerConsumer() {
+  producer();
+  consumer();
 }
 
